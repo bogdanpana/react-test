05a01857618a26565693de8278ceb2d8b38b9a12
Bogdan <bogdanpana@thecon.ro>
2020-09-24 11:12:39 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Navbar from './Components/Header/navbar.component';
 import Post from './Components/Post/post.component';
+import CustomButton from './Components/CustomButton/custom-button.component';
 
 class App extends Component {
   constructor(props) {
@@ -24,17 +25,54 @@ class App extends Component {
         
       }
   }
+  handleTitleChange = (event) =>{
+    this.setState({title: event.target.value})
+}
+  handleDescriptionChange = (event) =>{
+    this.setState({description: event.target.value})
+  }
+  addNewPost = () =>{
+    this.setState(prevState => ({
+      posts: [...prevState.posts, {title: this.state.title, description: this.state.description}]
+    })
+    )
+  }
+
   render() {
     return (
       <div>
-        <Navbar/>
-        <Post title="dada"/>
-        <Post title="dada2"/>
+        <Navbar />
+        <Post title="dada" />
+        <Post title="dada2" />
+        <div class="container">
+        {this.state.posts.map((item, index) => (
+          <div key={index}>            
+            <Post key={index} title={item.title} description={item.description} />
+            <p>{index}</p>
+          </div>
+        ))
+        }
+        </div>
 
-      {this.state.posts.map(item => (
-      <Post title={item.title} description ={item.description}/>
-      ))
-      }
+
+        <div style={{ display: "flex", flexDirection: "column", height: "600px", justifyContent: "space-around" }}>
+          {/* Title input */}
+          <label style={{ height: "100px", width: "300px", display: "flex", flexDirection: "column" }}>
+            Titlu:
+        <input type="text" name="title" value={this.state.title} onChange={this.handleTitleChange}></input>
+          </label>
+
+          {/* Content input */}
+          <label style={{ height: "300px", width: "300px", display: "flex", flexDirection: "column" }}>
+            Descriere:
+        <textarea cols="10" rows="3" name="description" style={{ resize: "none" }} value={this.state.description} onChange={this.handleDescriptionChange}></textarea>
+          </label>
+
+          <CustomButton onClick={this.addNewPost} name="ADD A NEW POST" />
+
+          <h1>Post Preview:</h1>
+          <Post title={this.state.title} description={this.state.description} />
+        </div>
 
       </div>
       );
